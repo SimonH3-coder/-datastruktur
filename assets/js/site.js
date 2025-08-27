e5ab61fe2f138f02008ceb837a5f352a4cc3b578
SimonH3-coder <simo16f4@elev.techcollege.dk>
2025-08-27 12:57:19 +0200

    Yellow1

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,32 +1,41 @@
 // write cool JS hwere!!
- const userSelect = document.getElementById("userSelect");
-    const nameInput = document.getElementById("nameInput");
-    const ageInput = document.getElementById("ageInput");
-    const colorInput = document.getElementById("colorInput");
-    const saveButton = document.getElementById("saveButton");
+const userSelect = document.getElementById("userSelect");
+const nameInput = document.getElementById("nameInput");
+const ageInput = document.getElementById("ageInput");
+const colorInput = document.getElementById("colorInput");
+const saveButton = document.getElementById("saveButton");
 
-    // Event listener: vis data for valgt bruger
-  function userSelectChange() {
-      const selectedUser = userSelect.value;
-       console.log('change user to: '+selectedUser);
-      // TODO: Hent data fra localStorage og vis i formularen
-      
-    };
+const dataBruger = {
+  bruger1: { name: "Pia", age: 24, color: "red" },
+  bruger2: { name: "Brian", age: 29, color: "green" },
+  bruger3: { name: "Nina", age: 21, color: "brun" },
+  bruger4: { name: "Michael", age: 26, color: "blue" },
+};
 
-    // Event listener: gem data fra formularen
-    saveButton.addEventListener("click", () => {
-        console.log("save data");
-        
-      const selectedUser = userSelect.value;
-      const name = nameInput.value;
-      const age = ageInput.value;
-      const color = colorInput.value;
+// Event listener: vis data for valgt bruger
+function userSelectChange() {
+  const selectedUser = userSelect.value;
+  console.log("change user to: " + selectedUser);
+  // TODO: Hent data fra localStorage og vis i formularen
+}
 
-      // TODO: Gem data i localStorage for den valgte bruger
+function saveBruger(bruger) {
+  localStorage.setItem("bruger", JSON.stringify(bruger));
+}
 
+fun;
 
+// Event listener: gem data fra formularen
+saveButton.addEventListener("click", () => {
+  console.log("save data");
 
-    });
+  const selectedUser = userSelect.value;
+  const name = nameInput.value;
+  const age = ageInput.value;
+  const color = colorInput.value;
 
-    // Vis data for første bruger ved load
-    window.onload = userSelectChange;
+  // TODO: Gem data i localStorage for den valgte bruger
+});
+
+// Vis data for første bruger ved load
+window.onload = userSelectChange;
